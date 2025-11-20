5b411ec49df4ae1530f888399a70be0e676aef11
artistatbl <jdaly2991@gmail.com>
2025-11-20 12:31:03 +0000

    feat(auth): add email export path in package.json

--- apps/feed/next.config.mjs
+++ apps/feed/next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@feedgot/auth"],
     images: {
         domains: [
           'lh3.googleusercontent.com', // Google authentication provider
